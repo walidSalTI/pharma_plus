e7e6d45c6f74fba504cd7eec9554f1cf055472e5
walidSalTI <walidalsalti652@gmail.com>
2026-06-09 19:03:47 +0300

    refactor: add all needed packages

--- server/bootstrap/app.php
+++ server/bootstrap/app.php
@@ -1,14 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function (){
+            Route::middleware('api')
+            ->prefix('api/v1')
+            ->group(__DIR__.'/../routes/api_v1.php');
+        }
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
